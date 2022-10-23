7bea11683f7ef4d8451b6d56ef2e454845f5822b
gsusha <x.dyachenko@gmail.com>
2022-10-23 07:15:58 +0300

    search field for hot and hidden
    (cherry picked from commit 0c3da5500821d1e298b20f62bd5febd9c3d36fb2)

--- lib/presentation/pages/home/hidden/hidden_fragment.dart
+++ lib/presentation/pages/home/hidden/hidden_fragment.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../widgets/base_text_field.dart';
 import '../../../widgets/tasks_board.dart';
 import '../tasks/bloc/tasks_bloc.dart';
 
@@ -12,7 +13,16 @@ class HiddenFragment extends StatelessWidget {
     return BlocBuilder<TasksBloc, TasksState>(
       builder: (context, state) {
         if (state is TasksSuccess) {
-          return TasksBoard(state.tasks.where((e) => e.isHidden).toList());
+          return Column(
+            children: [
+              const BaseTextField(
+                'Найти задачу...',
+                icon: Icon(Icons.search_rounded),
+              ),
+              const SizedBox(height: 20),
+              TasksBoard(state.tasks.where((e) => e.isHidden).toList()),
+            ],
+          );
         }
         return const Center(child: CircularProgressIndicator());
       },
